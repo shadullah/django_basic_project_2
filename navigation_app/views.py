459b8c9b48b805e1437eeac61cdf43222d12b736
hexaalif <shadullahsad9@gmail.com>
2023-12-14 03:25:52 +0600

    dynamic object loaded to ui

--- navigation_app/views.py
+++ navigation_app/views.py
@@ -2,7 +2,22 @@ from django.shortcuts import render
 
 # Create your views here.
 def about(req):
-    data = {'author': 'tabid', 'age': 20, 'list': [1,2,3]}
+    data = {'author': 'tabid', 'age': 20, 'list': [1,2,3],
+            'courses':[
+                  
+                  {
+                        'id':1,
+                        'name': 'Python',
+                        'fee': 5000
+                  },
+                  {
+                        'id':2,
+                        'name': 'Django',
+                        'fee': 600
+                  }
+            ]
+            
+            }
     return render(req, 'navigation/about.html', data)
 
 def contact(req):
